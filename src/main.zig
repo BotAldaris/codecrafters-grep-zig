bdd3d7d82c1acf6fa627d9366ed2e14f6fb47ca9
BotAldaris <biells2002@gmail.com>
2025-09-27 09:41:37 -0300

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 var stdin = std.fs.File.stdin().readerStreaming(&.{});
 
-fn matchPattern(input_line: []const u8, pattern: []const u8) bool {
+fn matchPattern(input_line: []const u8, pattern: []const u8, gpa: Allocator) !bool {
     if (pattern.len == 1) {
         return std.mem.indexOf(u8, input_line, pattern) != null;
     }
@@ -10,7 +10,7 @@ fn matchPattern(input_line: []const u8, pattern: []const u8) bool {
     while (i < pattern.len) {
         switch (pattern[i]) {
             '\\' => return matchEscape(input_line, pattern, i + 1),
-            '[' => return matchArray(input_line, pattern, i + 1),
+            '[' => return try matchArray(input_line, pattern, i + 1, gpa),
             else => @panic("Unhandled pattern"),
         }
     }
@@ -36,16 +36,41 @@ fn matchEscape(input_line: []const u8, pattern: []const u8, index: usize) bool {
         else => false,
     };
 }
-fn matchArray(input_line: []const u8, pattern: []const u8, index: usize) bool {
+fn matchArray(input_line: []const u8, pattern: []const u8, index: usize, gpa: Allocator) !bool {
+    var begin = index;
     var end = index;
-    for (pattern[index..]) |value| {
+    var negative = false;
+    if (pattern[index] == '^') {
+        begin += 1;
+        end += 1;
+        negative = true;
+    }
+    var toMatch = std.AutoHashMap(u8, void).init(gpa);
+    defer toMatch.deinit();
+    for (pattern[begin..]) |value| {
         switch (value) {
             ']' => break,
-            else => {},
+            else => try toMatch.put(value, {}),
         }
         end += 1;
     }
-    return std.mem.indexOfAny(u8, input_line, pattern[index..end]) != null;
+    if (pattern[end] != ']') {
+        return false;
+    }
+    if (negative) {
+        for (input_line) |c| {
+            if (!toMatch.contains(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    for (input_line) |c| {
+        if (toMatch.contains(c)) {
+            return true;
+        }
+    }
+    return false;
 }
 pub fn main() !void {
     var buffer: [1024]u8 = undefined;
@@ -66,11 +91,11 @@ pub fn main() !void {
     var input_buffer: [1024]u8 = undefined;
     const input_len = try stdin.read(&input_buffer);
     const input_slice = input_buffer[0..input_len];
-    // var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
-    // var gpa = debug_allocator.allocator();
-    // defer debug_allocator.deinit();
+    var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
+    const gpa = debug_allocator.allocator();
+    defer _ = debug_allocator.deinit();
     const pattern = args[2];
-    if (matchPattern(input_slice, pattern)) {
+    if (try matchPattern(input_slice, pattern, gpa)) {
         std.debug.print("matched", .{});
         std.process.exit(0);
     } else {
